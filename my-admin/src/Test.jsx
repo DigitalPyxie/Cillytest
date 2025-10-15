2c30bc65e1fc5c268277e35addcf907685d7fcfa
Kekeletso Liphoko <75853126+DigitalPyxie@users.noreply.github.com>
2025-10-15 10:55:27 +0000

    created temp files (test.jsx and index.jsx)

---
+++ my-admin/src/Test.jsx
@@ -0,0 +1,6 @@
+//Might break the app completely
+import MyAdmin from "./admin";
+
+const App = () => <MyAdmin />;
+
+export default App;
---
+++ my-admin/src/admin/index.jsx
@@ -0,0 +1,17 @@
+import { Admin, Resource, ListGuesser } from "react-admin";
+// import jsonServerProvider from "ra-data-json-server";
+import { createTrailbaseProvider } from "../ra-trailbase.js"; //where it's saved
+
+
+//const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
+const main_url = createTrailbaseProvider("https://special-halibut-7qv47r9rppqc7qp-4000.app.github.dev/");
+
+//dataProvider={dataProvider}
+const App = () => (
+  <Admin  main_url = {main_url}> 
+    <Resource name="posts" list={ListGuesser} />
+    <Resource name="comments" list={ListGuesser} />
+  </Admin>
+);
+
+export default App;
